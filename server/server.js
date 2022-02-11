b03531e9a3be0dd6e5e10285b9adb2e0fd40bdc4
Davey-M <davey.meuer@gmail.com>
2022-02-11 15:33:30 -0600

    Finished server side get route

--- server/server.js
+++ server/server.js
@@ -5,4 +5,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
 
+const notesRoute = require('./routes/notes.route');
+app.use('/notes', notesRoute);
+
 app.listen(port, () => console.log('App hosted on port:', port))
