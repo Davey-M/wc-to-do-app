9c45ac116fc432ca2acbeadefcbc1a6eff4dc88f
Davey-M <davey.meuer@gmail.com>
2022-02-11 15:39:31 -0600

    Added server comments

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+// host app on port specified by the hosing service or localhost
 const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
 
+// notes route handles sending and receiving notes from the database
 const notesRoute = require('./routes/notes.route');
 app.use('/notes', notesRoute);
 
