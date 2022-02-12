e55080a958278e18639c05cea33cebbdc2eef9ce
Davey-M <davey.meuer@gmail.com>
2022-02-12 12:36:30 -0600

    Added new database commands

--- database.sql
+++ database.sql
@@ -4,10 +4,25 @@ CREATE TABLE "notes" (
     "id" SERIAL PRIMARY KEY,
     "text" VARCHAR(255) NOT NULL,
     "completed" BOOLEAN NOT NULL,
-    "container" VARCHAR(255)
+    "position" INTEGER NOT NULL,
+    "container" INTEGER
 );
 
 INSERT INTO "notes" ("text", "completed", "container")
 VALUES 
-    ('Sample Note', false, '1'),
-    ('Another Sample', true, '1');
+    ('Sample Note', false, 1),
+    ('Another Sample', true, 1),
+    ('A third sample note.', false, 2);
+
+-- container database
+
+CREATE TABLE "containers" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL,
+    "position" INTEGER NOT NULL
+);
+
+INSERT INTO "containers" ("name", "position")
+VALUES
+    ('First Test Container', 1),
+    ('Second Test Container', 2);
