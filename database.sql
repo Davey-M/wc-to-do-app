3300d4316ba6a16497a1fd3ec92766ab60dcd033
Davey-M <davey.meuer@gmail.com>
2022-02-12 12:38:20 -0600

    Changed sql commands

--- database.sql
+++ database.sql
@@ -4,7 +4,6 @@ CREATE TABLE "notes" (
     "id" SERIAL PRIMARY KEY,
     "text" VARCHAR(255) NOT NULL,
     "completed" BOOLEAN NOT NULL,
-    "position" INTEGER NOT NULL,
     "container" INTEGER
 );
 
@@ -19,10 +18,9 @@ VALUES
 CREATE TABLE "containers" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(255) NOT NULL,
-    "position" INTEGER NOT NULL
 );
 
-INSERT INTO "containers" ("name", "position")
+INSERT INTO "containers" ("name")
 VALUES
-    ('First Test Container', 1),
-    ('Second Test Container', 2);
+    ('First Test Container'),
+    ('Second Test Container');
