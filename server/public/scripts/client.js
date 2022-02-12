067a9c800e7ba8fc8fd5375fd3c7c3f8cac642db
Davey-M <davey.meuer@gmail.com>
2022-02-11 22:29:30 -0600

    Added comments

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,6 +9,7 @@ function main() {
 const containers = [];
 // GLOBAL VARIABLE DECLARATIONS ------------------------------------------------
 
+// get notes from server
 function getNotes() {
 
     let options = {
@@ -18,6 +19,7 @@ function getNotes() {
 
     $.ajax(options)
         .then(response => {
+            // notes need to be rendered after being received from the server
             renderNotes(response.rows);
         })
         .catch(err => {
@@ -28,17 +30,23 @@ function getNotes() {
 function renderNotes(notes) {
     console.log(notes);
 
+    // used for validation when trying to add containers later
     for (let container of containers) {
         $(`#con-${container}`).empty();
     }
 
+    // loop through the notes
     for (let note of notes) {
+        // destructuring
         let { container, text, completed } = note;
 
+        // spaces break the ids. no notes will be added if there are spaces in the containers id
         let containerId = container.split(' ').join('-');
 
+        // create a container if one does not already exist on the dom
         if (!containers.includes(containerId)) {
             containers.push(containerId);
+            // the note-container id in this block breaks with spaces in the containerId
             $('#inner-container-container').append(`
                 <div class="container">
                     <h1>${container}</h1>
@@ -49,6 +57,7 @@ function renderNotes(notes) {
             `)
         }
 
+        // load the note into the dom
         $(`#con-${containerId}`).append(`
             <div class="note">
                 <p>${text}</p>
