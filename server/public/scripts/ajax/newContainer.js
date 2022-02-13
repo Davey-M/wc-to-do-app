4293754877532893069f3a6ee972b9eccd227f0e
Davey-M <davey.meuer@gmail.com>
2022-02-13 15:18:31 -0600

    add client post comments

--- server/public/scripts/ajax/newContainer.js
+++ server/public/scripts/ajax/newContainer.js
@@ -1,27 +1,31 @@
 function addContainer(e) {
     e.preventDefault();
 
+    // name is the only thing about the container that the user can control
     const name = e.target['name-input'].value;
 
+    // input verification
     if (!name) {
         alert('Containers must have names.');
         return;
     }
 
-    console.log(name);
+    // console.log(name); // Note for prod
 
     const options = {
         method: "POST",
         url: '/containers',
         data: {
+            // compact object structure
             name,
         }
     }
 
     $.ajax(options)
         .then(response => {
-            getContainers();
-            e.target.reset();
+            // getContainers runs getNotes when it is finished
+            getContainers(); // get.js
+            e.target.reset(); // reset the form
         })
         .catch(err => {
             console.error('Error with newContainer', err);
