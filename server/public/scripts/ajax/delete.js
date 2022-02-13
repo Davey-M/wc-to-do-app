ec4f7eb479465987af61c5c0a81798083281ac42
Davey-M <davey.meuer@gmail.com>
2022-02-13 16:26:45 -0600

    add sweet alerts

--- server/public/scripts/ajax/delete.js
+++ server/public/scripts/ajax/delete.js
@@ -4,25 +4,39 @@ function deleteNote() {
     console.log(id);
 
     // make sure the user didn't accidentally click the button
-    let userResponse = confirm('This note will be permanently deleted.');
-    if (userResponse === false) {
-        return;
-    }
+    // let userResponse = confirm('This note will be permanently deleted.');
+    // if (userResponse === false) {
+    //     return;
+    // }
 
-    // send delete request to database
-    const options = {
-        method: 'DELETE',
-        url: `/notes/${id}`,
-    }
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this note!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+            // send delete request to database
+            const options = {
+                method: 'DELETE',
+                url: `/notes/${id}`,
+            }
 
-    $.ajax(options)
-        .then(response => {
-            // getContainers runs getNotes when it is finished
-            getContainers(); // get.js
-        })
-        .catch(err => {
-            console.error(err);
-        })
+            $.ajax(options)
+                .then(response => {
+                    // getContainers runs getNotes when it is finished
+                    getContainers(); // get.js
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+
+        } else {
+          return;
+        }
+      });
 }
 
 function deleteContainer() {
@@ -31,21 +45,36 @@ function deleteContainer() {
     console.log(id);
 
     // make sure the user didn't accidentally click the button
-    let userResponse = confirm('This container will be permanently deleted.');
-    if (userResponse === false) {
-        return;
-    }
+    // let userResponse = confirm('This container will be permanently deleted.');
+    // if (userResponse === false) {
+    //     return;
+    // }
 
-    const options = {
-        method: 'DELETE',
-        url: `/containers/${id}`,
-    }
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this container!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
 
-    $.ajax(options)
-        .then(response => {
-            getContainers(); // get.js
-        })
-        .catch(err => {
-            console.error(err);
-        })
+            const options = {
+                method: 'DELETE',
+                url: `/containers/${id}`,
+            }
+        
+            $.ajax(options)
+                .then(response => {
+                    getContainers(); // get.js
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+
+        } else {
+          return;
+        }
+    });
 }
